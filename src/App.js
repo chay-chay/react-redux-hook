b50d914c0d7c6d58d027c47e9f8331215a2231ff
Chayanit <chayanit.chaisri@gmail.com>
2021-09-05 00:12:13 -0400

    Added thunk middleware & bindActionCreators for depositand withdraw money

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './store/index'
 
 function App() {
 // returning state inside of out store
 // const state = useSelector((state) => state)
 const account = useSelector((state) => state.account)
+const dispatch = useDispatch();
+// console.log(account)
 
-console.log(account)
+// const AC = bindActionCreators(actionCreators, dispatch)
+// destructuring 
+const {depositMoney, withdrawMoney} = bindActionCreators(actionCreators, dispatch)
+// console.log(AC)
+
 
   return (
     <div className="App">
     <h1>{account}</h1>
-      <button style={{margin: 10}}>Deposit</button>
-      <button style={{margin: 10}}>Dithdraw</button>
+      <button style={{margin: 10}} onClick={() => depositMoney(1000)}>Deposit</button>
+      <button style={{margin: 10}} onClick={() => withdrawMoney(1000)}>Dithdraw</button>
     </div>
   );
 }
