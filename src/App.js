a497c4f701b82b953703f03edcd39b45dc1c8587
Chayanit <chayanit.chaisri@gmail.com>
2021-09-04 23:45:37 -0400

    import useSelector to get state from store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+// returning state inside of out store
+// const state = useSelector((state) => state)
+const account = useSelector((state) => state.account)
+
+console.log(account)
+
   return (
     <div className="App">
       <button>deposit</button>
