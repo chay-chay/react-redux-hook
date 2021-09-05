e5b7f1394038d37305f9abc1d620dd4f55aafc83
Chayanit <chayanit.chaisri@gmail.com>
2021-09-04 22:51:00 -0400

    Added npm redux react-redux & create store & accountReducer

---
+++ src/store/reducers/accountReducer.js
@@ -0,0 +1,13 @@
+// reducer takes 2 parameters
+const reducer = (state = 0, action) => {
+    switch (action.type){
+        case "deposit":
+            return state + action.payload;
+        case "withdraw":
+            return state - action.payload;
+        default:
+            return state
+    }
+
+}
+export default reducer;
